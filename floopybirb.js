da56d5495ccc09caeb24fcd2989a0ecd35554d67
Fly2Freedom <evanninja3@gmail.com>
2021-01-19 11:31:51 -0500

    organization

--- floopybirb.js
+++ floopybirb.js
@@ -20,32 +20,26 @@ function exeptionsRand(intOrOut,max,min) {
   if (intOrOut == "in") {
     if (max >= randNumber && randNumber >= min) {
         inBool = false;
-    }else{
+    } else {
     randNumber = Math.floor(Math.random()*(max-min)) + min;
     }
-  }else if (intOrOut == "out") {
+  } else if (intOrOut == "out") {
     if (min >= randNumber <= max) {
         inBool = false;
-    }else{
+    } else {
           randNumber = Math.floor(Math.random()*max)-min;
-
+      }
     }
-    //code
-  }
   }
   return randNumber;
 }
+
 var pipeWalls = {north:undefined,east:undefined,south:undefined,west:undefined};
 var pipx = exeptionsRand("in", myCanvas.width-30, 75); //I haven't seen any change after modifying these numbers.
 var pipy = exeptionsRand("in", myCanvas.height-35,90); //Nothing changes here as well.
 var pipBotObj = {width:45,height:450, x:pipx ,y:pipy,gap: 40}; //Sets the bottom pipe's width (which is always the same) and height (which will randomly change). Changing the 'gap' value will change how far apart the pipes are.
 var pipTopObj = {width: 45,height:pipy-(pipBotObj.gap*2), x:pipx,y: 0}; //Does the same as above, but the pipe's height is based on the bottom pipes 'gap' value (in this case, the pipe's height will max out at 40 units above the bottom pipe).
 
-
-var gravity = 0.2; //Sets the gravity pulling the ball to the ground.
-var damping = 0.01; //The rate at which the ball slows down.
-var traction = 0.95; //Will make the ball stop.
-var ballSize = 20; //Sets the circle's radius.
 function drawPipes(){ //This function is for making sure the pipes aren't drawn off-screen where they are not needed.
     ctx.beginPath();
     if ((pipTopObj.x +pipTopObj.width) == 0) {
@@ -63,7 +57,10 @@ function drawPipes(){ //This function is for making sure the pipes aren't drawn 
   ctx.stroke();
 }
 
-
+var gravity = 0.2; //Sets the gravity pulling the ball to the ground.
+var damping = 0.01; //The rate at which the ball slows down.
+var traction = 0.95; //Will make the ball stop.
+var ballSize = 20; //Sets the circle's radius.
 
 function drawCircle() {
   ctx.beginPath();
@@ -107,6 +104,6 @@ function keyPress(e) { //Function that will play out when a key is pressed (e is
       //if (dx < 0) {
       //  dx-=5;
       //}
-      dy-=10; //Will make the ball jump a small distance.
+      dy-=5; //Will make the ball jump a small distance.
     }
 }
