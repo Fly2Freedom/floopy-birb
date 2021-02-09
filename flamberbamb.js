2752738d9e6e5c7de0b8bcb1859355eb2d6a6300
Rhydog3 <rhylie.williams@gmail.com>
2021-02-09 11:25:17 -0500

    hope you dont mind i made some changes

--- flamberbamb.js
+++ flamberbamb.js
@@ -20,37 +20,6 @@ function drawCircle() {
   ctx.stroke();
 }
 
-function component(width, height, color, x, y, type) {
-  this.type = type;
-  if (type == "image") {
-    this.image = new Image();
-    this.image.src = color;
-  }
-  this.width = width;
-  this.height = height;
-  this.speedX = 0;
-  this.speedY = 0;
-  this.x = x;
-  this.y = y;
-  this.update = function() {
-    ctx = myGameArea.context;
-    if (type == "image") {
-      ctx.drawImage(this.image,
-        this.x,
-        this.y,
-        this.width, this.height);
-    } else {
-      ctx.fillStyle = color;
-      ctx.fillRect(this.x, this.y, this.width, this.height);
-    }
-  }
-}
-
-function startGame() {
-  myGamePiece = new component(20, 20, "smileyface.png", 5, 5, "image");
-  myGameArea.start();
-}
-
 /* makePipe(lowRectX, lowRectY, lowRectWid, lowRectHeight, upRectX, upRectY, upRectWid, upRectHeight)
 @param lowRectX {obj}- x position of the long, bottom tube
 @param lowRectY {obj}- y position of the long, bottom tube
